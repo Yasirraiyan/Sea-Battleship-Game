19b3fd8bc3d74b25ccb1fe5b789406e6f28237b1
Yasirraiyan <127099527+Yasirraiyan@users.noreply.github.com>
2024-12-03 23:08:49 +0600

    Create Game Rules.java

---
+++ Game Rules.java
@@ -0,0 +1,14 @@
+public class GameRules
+{
+    public boolean isValidMove(int x, int y, Board board) 
+    {
+        
+        return true; 
+    }
+
+    public boolean checkWinCondition(Board board) 
+    {
+        
+        return false; 
+    }
+}
